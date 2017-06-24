0ed284f5fa328268add12d8a8e50cf93761f974f
John Burgess <john.burgess.74@alumni.brown.edu>
2017-06-24 15:16:56 -0400

    add ToDolist

---
+++ ToDolist/ToDolist/TD2AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TD2AppDelegate.h
+//  ToDolist
+//
+//  Created by John Burgess on 11/16/13.
+//  Copyright (c) 2013 JohnBurgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TD2AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDolist/ToDolist/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDolist
+//
+//  Created by John Burgess on 11/16/13.
+//  Copyright (c) 2013 JohnBurgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TD2AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TD2AppDelegate class]));
+    }
+}
